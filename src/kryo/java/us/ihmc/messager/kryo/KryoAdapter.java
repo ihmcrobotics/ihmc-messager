9ae1199ddf51106161adee83baaaf217eec2a730
Duncan Calvert <dcalvert@ihmc.us>
2019-03-04 11:48:43 -0600

    Clean up KryoAdapter so there's never more than one lambda in a line because that's super confusing.

--- src/kryo/java/us/ihmc/messager/kryo/KryoAdapter.java
+++ src/kryo/java/us/ihmc/messager/kryo/KryoAdapter.java
@@ -12,18 +12,19 @@ import java.util.function.BooleanSupplier;
 import java.util.function.Consumer;
 
 /**
- * Pretty much all that ihmc-communication does :rolling_on_floor_laughing:
+ * Unifies the API of Kryonet Server and Client.
  */
 public class KryoAdapter
 {
    private Listener kryoListener = new KryoListener();
-   private final BooleanSupplier isConnectedSupplier;
-   private final Consumer tcpSender;
-   private final Runnable connector;
-   private final Runnable disconnector;
-   private final Runnable updater;
-   private final ArrayList<Consumer> connectionStateListeners = new ArrayList<>();
    private Consumer receivedConsumer;
+   private final ArrayList<Consumer> connectionStateListeners = new ArrayList<>();
+
+   private final BooleanSupplier isConnectedSupplier;
+   private final RunnableThatThrows updater;
+   private final RunnableThatThrows connector;
+   private final RunnableThatThrows disconnector;
+   private final Consumer tcpSender;
 
    public KryoAdapter(int tcpPort)
    {
@@ -31,8 +32,8 @@ public class KryoAdapter
       server.addListener(kryoListener);
       server.getKryo().setRegistrationRequired(false);
       isConnectedSupplier = () -> server.getConnections().length > 0;
-      updater = () -> ExceptionTools.handle(() -> server.update(250), DefaultExceptionHandler.RUNTIME_EXCEPTION);
-      connector = () -> kickstarter(() -> server.bind(tcpPort));
+      updater = () -> server.update(250);
+      connector = () -> server.bind(tcpPort);
       disconnector = () -> server.close();
       tcpSender = message -> server.sendToAllTCP(message);
    }
@@ -43,8 +44,8 @@ public class KryoAdapter
       client.addListener(kryoListener);
       client.getKryo().setRegistrationRequired(false);
       isConnectedSupplier = () -> client.isConnected();
-      updater = () -> ExceptionTools.handle(() -> client.update(250), DefaultExceptionHandler.RUNTIME_EXCEPTION);
-      connector = () -> kickstarter(() -> client.connect(5000, serverAddress, tcpPort));
+      updater = () -> client.update(250);
+      connector = () -> client.connect(5000, serverAddress, tcpPort);
       disconnector = () -> client.close();
       tcpSender = message -> client.sendTCP(message);
    }
@@ -72,17 +73,31 @@ public class KryoAdapter
 
    public void connect()
    {
-      connector.run();
+      new Thread(() -> startNonBlockingConnect()).start();  // this is the "kickstart" method required to
+      new Thread(() -> waitForConnection()).start();        // get Kryo to connect
+   }
+
+   private void startNonBlockingConnect()
+   {
+      ExceptionTools.handle(() -> connector.run(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
+   }
+
+   private void waitForConnection()
+   {
+      while (!isConnectedSupplier.getAsBoolean())
+      {
+         ExceptionTools.handle(() -> updater.run(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
+      }
    }
 
    public void disconnect()
    {
-      disconnector.run();
+      ExceptionTools.handle(() -> disconnector.run(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
    }
 
    public void update()
    {
-      updater.run();
+      ExceptionTools.handle(() -> updater.run(), DefaultExceptionHandler.RUNTIME_EXCEPTION);
    }
 
    public void sendTCP(Object object)
@@ -103,33 +118,5 @@ public class KryoAdapter
    public void addConnectionStateListener(Consumer<Boolean> connectionStateListener)
    {
       connectionStateListeners.add(connectionStateListener);
-   }
-
-   /** Imagine having to kick a rusty lawn mower to start it, then you understand this method. */
-   private void kickstarter(RunnableThatThrows connect)
-   {
-      new Thread(() -> {
-         try
-         {
-            connect.run();
-         }
-         catch (Throwable e)
-         {
-            throw new RuntimeException(e);
-         }
-      }).start();
-      new Thread(() -> {
-         try
-         {
-            while (!isConnectedSupplier.getAsBoolean())
-            {
-               updater.run();
-            }
-         }
-         catch (Throwable e)
-         {
-            throw new RuntimeException(e);
-         }
-      }).start();
    }
 }
