1997d805bc4465e59f3b6a4d0b6e69b100555a9b
ds58 <30220598+ds58@users.noreply.github.com>
2025-02-05 13:21:23 -0600

    :arrow_up: ihmc-commons 0.35.1 (#9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ ihmc {
 
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
-   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:log-tools:0.6.5")
 }
 
@@ -37,5 +37,5 @@ examplesDependencies {
 testDependencies {
    api(ihmc.sourceSetProject("kryo"))
    api(ihmc.sourceSetProject("examples"))
-   api("us.ihmc:ihmc-commons-testing:0.34.0")
+   api("us.ihmc:ihmc-commons-testing:0.35.1")
 }
